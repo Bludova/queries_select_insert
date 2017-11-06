d7f2c8170ef7211d2f1a9fc0a3eb398fe59977f8
Bludova <32148601+Bludova@users.noreply.github.com>
2017-11-06 18:23:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,124 +1,120 @@
 <?php
-include './config.php';
-$connection = new mysqli($host, $user, $password, $db);
-$connection->set_charset('utf8');
-if(!$connection) 
-{
-  echo 'Не удолось подключится к базе данных!<br>';
-  echo mysqli_connect_error();
-  exit();
-} 
-    //$news->execute(['id']);
-// var_dump($news);
+  include './config.php';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
     <title>Запросы SELECT, INSERT, UPDATE и DELETE</title>
-        <style>
-    table { 
+    <style>
+      table { 
         border-spacing: 0;
         border-collapse: collapse;
-    }
+      }
 
-    table td, table th {
+      table td, table th {
         border: 1px solid #ccc;
         padding: 5px;
-    }
+      }
     
-    table th {
+      table th {
         background: #eee;
-    }
-</style>
-</head>
- <body>
- <?php
-if(isset($_GET["action"]) ==='edit'){
-    echo $_POST['saves'];
-mysqli_query($connection, "UPDATE `tasks`SET `description`='".$_POST['saves']."'  WHERE `id` = '".$_GET['id']."' ");
-     ?>
-        <form method="POST">
-        <input type="text" name="saves" placeholder="" value="<?php $_POST['saves']?>">
-        <input type="submit" name="save" value="Сохранить">
-    </form>
+      }
+    </style>
+  </head>
+  <body>
     <?php
-}else {
-?>
-   <form method="POST">
-        <input type="text" name="description" placeholder="Описание задачи" value="">
-        <input type="submit" name="save" value="Добавить">
-    </form>
-<?php }?>
-<hr>
-<!--  <form method="POST">
-        <label for="sort">Сортировать по:</label>
-        <select name="sort_by">
-            <option value="date_created">Дате добавления</option>
-            <option value="is_done">Статусу</option>
-            <option value="description">Описанию</option>
-        </select>
-        <input type="submit" name="sort" value="Отсортировать" />
-    </form> -->
-    <hr>
+// подключение к db
+      try {
+        $pdo = new PDO(
+        'mysql:host=localhost;dbname=global',
+        $user,
+        $password,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+      }
+      catch (PDOException $e) {
+        echo "Невозможно установить соединение с базой данных";
+        exit();
+      }
+    ?>
 
-<table>
-    <tr>
+    <form method="POST">
+      <input type="text" name="description" placeholder="Описание задачи" value="">
+      <input type="submit" name="save" value="Добавить">
+    </form>
+
+    <table>
+      <tr>
         <th>Описание задачи</th>
         <th>Дата добавления</th>
         <th>Статус</th>
         <th>Изменить</th>
-    </tr>
- <?php
- $sql = "SELECT * FROM tasks";
-$result = $connection ->query($sql);
-foreach ($result as $row) {
-    $id=$row['id'];
-    if($row['is_done'] == 0){
-       $is_done = 'В процессе';
-    } else{
-        $is_done = 'Выполнено';
-    }
+      </tr>
+      <?php
+ //вывод всех данных
+        $sql = "SELECT * FROM tasks";
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
 
-    ?>
-<tr> <td><?php echo $row['description'] . "</td><td>". $row['date_added'] . "</td><td>". $is_done ;?>
- </td> </td> <td> <a href="?id=<?=$row['id'];?>&action=edit">Изменить</a> <a href="?id=<?=$row['id'];?>&action=done">Выполнить</a> <a href="?id=<?=$row['id'];?>&action=delete">Удалить</a></td></tr>
-<?php
-}
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+          $result[] = $row;
+        }
+    
+        foreach ($result as $key => $value) {
+          $id = $value['id'];
+          $id = intval($id);
 
-?>
-</table>
-<?php
+          if($value['is_done'] == 0){
+            $is_done = 'В процессе';
+          } else{
+            $is_done = 'Выполнено';
+          }
+      ?>
 
-var_dump($_POST);
-if (isset($_POST['save']) ){
-      if($_POST['description'] == '')
-       {
-            echo'Введите описание задачи';
-       } else {
-   $description = $_POST['description'];
+          <tr>
+            <td><?=$value['description'];?></td>
+            <td><?=$value['date_added'];?></td>
+            <td><?=$is_done;?></td>
+            <td><a href="?id=<?="$id";?>&action=done">Выполнить</a> <a href="?id=<?=$id;?>&action=delete">Удалить</a></td>
+            <!-- <a href="?id=<?=$id;?>&action=edit">Изменить</a> -->
+          </tr>
 
-    mysqli_query($connection, "INSERT INTO `tasks` (`id`, `description`, `is_done`, `date_added`) VALUES (NULL,'".$_POST['description']."', '0', CURRENT_TIMESTAMP)");
-}
-    // "INSERT INTO `tasks` (`id`, `description`, `is_done`, `date_added`) VALUES (NULL, '".$_POST['description']."', '0', CURRENT_TIMESTAMP)";
-   // $add = "INSERT INTO tasks (`id`, `description`, `is_done`, `date_added`) VALUES (NULL, 'dfd', '0')";
-   // $resultadd = $connection ->query($add);
-   // $add = "INSERT INTO tasks (`id`, `description`, `is_done`, `date_added`) VALUES (NULL," .$description.", '0', CURRENT_TIMESTAMP");
-   // echo "$description <hr>";
-}
-// $gets_del = $_GET;
-$id = $_GET["id"];
-foreach ($gets_del as $key => $value) {
-}
-if($_GET["action"] ==='delete'){
-     mysqli_query($connection, "DELETE FROM `tasks` WHERE `id` = '".$_GET['id']."' ");
-}
+      <?php
+        }
 
-if($_GET["action"] ==='done'){
-     echo 'don';
-      mysqli_query($connection, "UPDATE `tasks`SET `is_done`='1' WHERE `id` = '".$_GET['id']."' ");
-}
-?>
-<a href="index.php?id=<?=$items["id"]?>">Удалить </a>` 
-</body>
+        if(count($_POST > 0)){
+          $description = trim($_POST['description']);
+          $description = htmlspecialchars($description);
+          $is_done = 0;
+          $is_done = intval($is_done);
+
+          if($description != ''){
+            $query = $pdo->prepare("INSERT INTO `tasks` (`id`, `description`, `is_done`, `date_added`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP)");
+            $params = [$description, $is_done];
+            $query->execute($params);
+            //header("Location: index.php");
+            //exit();
+          }
+        }
+
+        $idEdit = $_GET["id"];
+        //$idEdit = trim($idEdit);
+        $idEdit = intval($idEdit);
+// Удалить
+        if($_GET["action"] ==='delete'){
+          $queryEdit = $pdo->prepare("DELETE FROM `tasks` WHERE `id` = ? ");
+          $paramsEdit = [$idEdit];
+          $queryEdit->execute($paramsEdit);
+        }
+// Выполнить
+        if($_GET["action"] ==='done'){
+          $queryEdit = $pdo->prepare("UPDATE `tasks`SET `is_done`='1' WHERE `id` = ?" );
+          $paramsEdit = [$idEdit];
+          $queryEdit->execute($paramsEdit);
+        }
+
+      ?>
+      
+    </table>
+  </body>
 </html>
